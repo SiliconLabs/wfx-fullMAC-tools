05239aecb8d06c52336ff9901b68201f11e96a64
Julien Tiron <julien.tiron@silabs.com>
2019-09-24 17:04:30 +0200

    Add FW 3.0.0 to the STM32 examples

---
+++ Examples/STM32/Projects/WF200_driver_F429ZI_FreeRTOS-LwIP/Src/dhcp_client.c
@@ -0,0 +1,154 @@
+/**
+  ******************************************************************************
+  * @file    dhcp_client.c 
+  * @brief   Ethernet specefic module
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright © 2017 STMicroelectronics International N.V. 
+  * All rights reserved.</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include "lwip/dhcp.h"
+#include "dhcp_client.h"
+#include "lwip_freertos.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+#define MAX_DHCP_TRIES  4
+volatile uint8_t DHCP_state = DHCP_OFF;
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Notify the User about the network interface config status
+  * @param  netif: the network interface
+  * @retval None
+  */
+void User_notification(int link_up) 
+{
+  if (link_up)
+  {
+    DHCP_state = DHCP_START;
+  }
+  else
+  {  
+    /* Update DHCP state machine */
+    DHCP_state = DHCP_LINK_DOWN;
+  } 
+}
+
+/**
+  * @brief  DHCP Process
+  * @param  argument: network interface
+  * @retval None
+  */
+void DHCP_thread(void const * argument)
+{
+  struct netif *netif = (struct netif *) argument;
+  ip_addr_t ipaddr;
+  ip_addr_t netmask;
+  ip_addr_t gw;
+  struct dhcp *dhcp;
+  
+  for (;;)
+  {
+    switch (DHCP_state)
+    {
+    case DHCP_START:
+      {
+        ip_addr_set_zero_ip4(&netif->ip_addr);
+        ip_addr_set_zero_ip4(&netif->netmask);
+        ip_addr_set_zero_ip4(&netif->gw);       
+        dhcp_start(netif);
+        DHCP_state = DHCP_WAIT_ADDRESS;
+      }
+      break;
+      
+    case DHCP_WAIT_ADDRESS:
+      {                
+        if (dhcp_supplied_address(netif)) 
+        {
+          DHCP_state = DHCP_ADDRESS_ASSIGNED;	
+          printf("IP address : %d.%d.%d.%d\r\n",
+                  sta_netif.ip_addr.addr & 0xff,
+                  (sta_netif.ip_addr.addr >> 8) & 0xff,
+                  (sta_netif.ip_addr.addr >> 16) & 0xff,
+                  (sta_netif.ip_addr.addr >> 24) & 0xff);
+        }
+        else
+        {
+          dhcp = (struct dhcp *)netif_get_client_data(netif, LWIP_NETIF_CLIENT_DATA_INDEX_DHCP);
+
+          /* DHCP timeout */
+          if (dhcp->tries > MAX_DHCP_TRIES)
+          {
+            DHCP_state = DHCP_TIMEOUT;
+            
+            /* Stop DHCP */
+            dhcp_stop(netif);
+            
+            /* Static address used */
+            IP_ADDR4(&ipaddr, sta_ip_addr0 , sta_ip_addr1 , sta_ip_addr2 , sta_ip_addr3 );
+            IP_ADDR4(&netmask, sta_netmask_addr0, sta_netmask_addr1, sta_netmask_addr2, sta_netmask_addr3);
+            IP_ADDR4(&gw, sta_gw_addr0, sta_gw_addr1, sta_gw_addr2, sta_gw_addr3);
+            netif_set_addr(netif, ip_2_ip4(&ipaddr), ip_2_ip4(&netmask), ip_2_ip4(&gw));
+            
+          }
+        }
+      }
+      break;
+  case DHCP_LINK_DOWN:
+    {
+      /* Stop DHCP */
+      dhcp_stop(netif);
+      DHCP_state = DHCP_OFF; 
+    }
+    break;
+    default: break;
+    }
+    
+    /* wait 250 ms */
+    osDelay(250);
+  }
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

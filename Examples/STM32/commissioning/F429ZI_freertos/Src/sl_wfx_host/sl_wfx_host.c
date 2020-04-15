0787db35a17261dff497c069268758ecc4a41356
Julien Tiron <julien.tiron@silabs.com>
2020-04-15 11:52:10 +0200

    update error/exception reporting in every example

--- Examples/STM32/commissioning/F429ZI_freertos/Src/sl_wfx_host/sl_wfx_host.c
+++ Examples/STM32/commissioning/F429ZI_freertos/Src/sl_wfx_host/sl_wfx_host.c
@@ -253,21 +253,34 @@ sl_status_t sl_wfx_host_post_event(sl_wfx_generic_message_t *event_payload)
     }
   case SL_WFX_EXCEPTION_IND_ID:
     {
-      printf("Firmware Exception\r\n");
       sl_wfx_exception_ind_t *firmware_exception = (sl_wfx_exception_ind_t*)event_payload;
-      printf ("Exception data = ");
-      for(int i = 0; i < SL_WFX_EXCEPTION_DATA_SIZE; i++)
-      {
-        printf ("%X, ", firmware_exception->body.data[i]);
+      uint32_t data_length = firmware_exception->header.length - sizeof(sl_wfx_header_t);
+      uint8_t *exception_body = (uint8_t *)&firmware_exception->body;
+      printf("firmware exception %lu\r\n", firmware_exception->body.reason);
+      for (uint16_t i = 0; i < data_length; i += 16) {
+        printf("dump: %.8x:", i);
+        for (uint8_t j = 0; (j < 16) && ((i + j) < data_length); j ++) {
+            printf(" %.2x", *exception_body);
+            exception_body++;
+        }
+        printf("\r\n");
       }
-      printf("\r\n");
       break;
     }
   case SL_WFX_ERROR_IND_ID:
     {
-      printf("Firmware Error\r\n");
       sl_wfx_error_ind_t *firmware_error = (sl_wfx_error_ind_t*)event_payload;
-      printf ("Error type = %lu\r\n",firmware_error->body.type);
+      uint32_t data_length = firmware_error->header.length - sizeof(sl_wfx_header_t);
+      uint8_t *error_body = (uint8_t *)&firmware_error->body;
+      printf("firmware error %lu\r\n", firmware_error->body.type);
+      for (uint16_t i = 0; i < data_length; i += 16) {
+        printf("dump: %.8x:", i);
+        for (uint8_t j = 0; (j < 16) && ((i + j) < data_length); j ++) {
+            printf(" %.2x", *error_body);
+            error_body++;
+        }
+        printf("\r\n");
+      }
       break;
     }
   }
